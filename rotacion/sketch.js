667e5fc1ea83774b4398905ff41a2727b949f4be
karlos g liberal <karlosgliberal@gmail.com>
2019-02-22 13:55:18 +0100

    comentarios en sketch

--- rotacion/sketch.js
+++ rotacion/sketch.js
@@ -1,22 +1,14 @@
-//Tranformaciones genekogan
-//http://genekogan.com/code/p5js-perlin-noise/
-
 /**
- * generates grid
- *
- * s                   : save png
+ * Sketch básico sobre la animación
+ * https://developer.mozilla.org/es/docs/Web/API/Window/requestAnimationFrame
  */
-
 function setup() {
   createCanvas(400, 400);
   background(240);
-  frameRate(1);
-
-  //rectMode(CENTER); // now the first two arguments of a rect are its center point, not corner
 }
+
 function draw() {
   background(240);
-
   // move the origin to the pivot point
   translate(150, 150);
 
@@ -25,15 +17,16 @@ function draw() {
   rotate(radians(frameCount));
   // and draw the square at the origin
   fill(0);
-  //rotate(cos(frameCount + noise(frameCount * 0.02)));
   rect(0, 0, 100, 100);
-
-  // rotate(radians(frameCount));
-  // // and draw the square at the origin
-  // fill(0);
-  // rect(0, 0, 100, 100);
 }
 
 function keyPressed() {
   if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
 }
+
+//frameRate(1);
+//rectMode(CENTER); // now the first two arguments of a rect are its center point, not corner
+// rotate(radians(frameCount));
+// // and draw the square at the origin
+// fill(0);
+// rect(0, 0, 100, 100);
